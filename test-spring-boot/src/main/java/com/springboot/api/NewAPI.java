4bb0c4fc84a3d6e736e65f7e30e98571c86aaf5e
XuShiTa <XuShiTa@192.168.1.17>
2021-08-22 23:35:25 +0700

    init springboot, add jpa hibernate

---
+++ test-spring-boot/src/main/java/com/springboot/api/NewAPI.java
@@ -0,0 +1,28 @@
+package com.springboot.api;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.dto.NewDTO;
+
+@RestController
+public class NewAPI {
+
+	@PostMapping("/new")
+	public NewDTO createNew(@RequestBody NewDTO model) {
+		return model;
+	}
+	
+	@PutMapping("/new")
+	public NewDTO updateNew(@RequestBody NewDTO model) {
+		return model;
+	}
+	
+	@DeleteMapping("/new")
+	public void deleteNew(@RequestBody Long[] ids) {
+		
+	}
+}
